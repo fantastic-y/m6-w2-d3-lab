c50e2fb01dede91fbd1793e103f94f2bf299ae5b
Fan <52841919+fantastic-y@users.noreply.github.com>
2021-11-29 18:25:21 -0800

    fixed the problem

--- react-app/src/BookEdit.js
+++ react-app/src/BookEdit.js
@@ -32,7 +32,7 @@ class BookEdit extends Component {
         event.preventDefault();
         const {item} = this.state;
 
-        await fetch('http://localhost:8080/api/booklists', {
+        await fetch('http://localhost:8080/api/booklist', {
             method: (item._id) ? 'PUT' : 'POST',
             headers: {
                 'Accept': 'application/json',
@@ -40,65 +40,65 @@ class BookEdit extends Component {
             },
             body: JSON.stringify(item),
         });
-        this.props.history.push('/booklists');
+        this.props.history.push('/');
     }
     render() {
         const {item} = this.state;
-        const title =
+        const pagetitle =
             <h2 className="mt-3">
                 {/* if item has an id number, otherwise */}
                 {item._id ? 'Edit Book' : 'Add Book'}
             </h2>;
-            return(
-                <div>
-                    <Container>
-                        {/* display the appropriate title */}
-                        {title}
-                        <Form onSubmit={this.handleSubmit}>
-                            <FormGroup>
-                                <Label
-                                    for="title"
-                                    className="h5 mt-3"
-                                >Book Title</Label>
-                                <Input
-                                    type="text"
-                                    name="title"
-                                    id="title"
-                                    value={item.title || ''}
-                                    onChange={this.handleChange}
-                                    autoComplete="title"
-                                />
-                            </FormGroup>
-                            <FormGroup>
-                                <Label
-                                    for="author"
-                                    className="h5 mt-3"
-                                >Author</Label>
-                                <Input
-                                    type="text"
-                                    name="author"
-                                    id="author"
-                                    value={item.author || ''}
-                                    onChange={this.handleChange}
-                                    autoComplete="author"
-                                />
-                            </FormGroup>
-                            <FormGroup>
-                                <Button
-                                    color="primary"
-                                    type="submit"
-                                    className="mt-3"
-                                >Save</Button>{' '}
-                                <Button
-                                    color="secondary"
-                                    className="mt-3"
-                                    tag={Link} to="/"
-                                >Cancel</Button>
-                            </FormGroup>
-                        </Form>
-                    </Container>
-                </div>
-            )
+        return(
+            <div>
+                <Container>
+                    {/* display the appropriate title */}
+                    {pagetitle}
+                    <Form onSubmit={this.handleSubmit}>
+                        <FormGroup>
+                            <Label
+                                for="title"
+                                className="h5 mt-3"
+                            >Book Title</Label>
+                            <Input
+                                type="text"
+                                name="title"
+                                id="title"
+                                value={item.title || ''}
+                                onChange={this.handleChange}
+                                autoComplete="title"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label
+                                for="author"
+                                className="h5 mt-3"
+                            >Author</Label>
+                            <Input
+                                type="text"
+                                name="author"
+                                id="author"
+                                value={item.author || ''}
+                                onChange={this.handleChange}
+                                autoComplete="author"
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <Button
+                                color="primary"
+                                type="submit"
+                                className="mt-3"
+                            >Save</Button>{' '}
+                            <Button
+                                color="secondary"
+                                className="mt-3"
+                                tag={Link} to="/"
+                            >Cancel</Button>
+                        </FormGroup>
+                    </Form>
+                </Container>
+            </div>
+        )
     }
 }
 
